620b58ea1f593e1d7384cd87f446586c504d611f
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-03-29 04:30:21 -0700

    Added button for finding a movie

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
 function App() {
-  
+    const findMovie = () => {
+
+    }
+
+    return (
+        <div className="App">
+            <button onClick={findMovie}>Find Me a Movie!</button>
+        </div>
+    )
 }
 
 export default App;
