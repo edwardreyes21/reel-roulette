304663387f32e801b98658dd2f69d6336396ab21
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-03-29 17:34:07 -0700

    Created GET request for TMDB's Discover endpoint

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import axios from 'axios';
 
 function App() {
     const findMovie = () => {
-
+        const api_key = process.env.REACT_APP_TMDB_API_KEY;
+        
+        const movie_details = axios
+            .get(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US`)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     return (
