7dd5c0a7e114f3a66ebd79bbbcc2d820dc49f919
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-04-01 00:34:07 -0700

    Create helper function for getting proper page

--- src/App.js
+++ src/App.js
@@ -4,27 +4,47 @@ import axios from 'axios';
 function App() {
     const [genre, setGenre] = useState([]);
     const [maxYear, setMaxYear] = useState(new Date().getFullYear());
-    const [minYear, setMinYear] = useState(1930);
+    const [minYear, setMinYear] = useState(2023);
 
-    const findMovie = () => {
+    const findMovie = async () => {
         const api_key = process.env.REACT_APP_TMDB_API_KEY;
-        const total_pages = 500;
+        const default_total_pages = 500; // By design, TMDB API can only retrieve up to the 500th page
 
-        const random_page = Math.floor(Math.random() * total_pages + 1);
-        console.log("Random page: " + random_page);
-        
-        axios
-            .get(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&page=${random_page}
+        const findRandomMovie = async (minYear, maxYear, page) => {
+            return axios
+            .get(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&page=${page}
                     &primary_release_date.gte=${minYear}-01-01&primary_release_date.lte=${maxYear}-12-31`)
-            .then((response) => {
+            .then(async (response) => {
                 console.log(response);
 
-                const selected_movie = response.data.results[0];
-                console.log("Selected movie: " + JSON.stringify(selected_movie, null, 2));
+                const actual_total_pages = response.data.total_pages;
+
+                if (actual_total_pages < default_total_pages) {
+                    const new_page = Math.floor(Math.random() * actual_total_pages + 1);
+                    console.log("Actual pages is lower than total pages");
+                    return await findRandomMovie(minYear, maxYear, new_page);
+                }
+                else {
+                    const selected_movie = response.data.results[0];
+                    console.log("Default pages is correct, returning movie");
+                    return selected_movie;
+                }
             })
             .catch((error) => {
                 console.log(error);
             });
+        }
+
+        const random_page = Math.floor(Math.random() * default_total_pages + 1);
+        console.log("Random page: " + random_page);
+
+        const selected_movie = await findRandomMovie(minYear, maxYear, random_page);
+
+        if (selected_movie) {
+            console.log("Selected movie: " + JSON.stringify(selected_movie, null, 2));
+        } else {
+            console.log("No movie with that filter is fetched");
+        } 
     }
 
     return (
