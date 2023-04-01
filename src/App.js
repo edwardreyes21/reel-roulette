a5d67ac5fd97184921a00e5ebfaab8d2054cc634
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-03-31 22:41:17 -0700

    Reorganized code for getting random page

--- src/App.js
+++ src/App.js
@@ -4,28 +4,15 @@ import axios from 'axios';
 function App() {
     const findMovie = () => {
         const api_key = process.env.REACT_APP_TMDB_API_KEY;
+        const total_pages = 500;
+
+        const random_page = Math.floor(Math.random() * total_pages + 1);
+        console.log("Random page: " + random_page);
         
         axios
-            .get(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US`)
+            .get(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&page=${random_page}`)
             .then((response) => {
                 console.log(response);
-                const total_pages = 500;
-
-                // Get total pages from JSON object
-                // Using total pages, get a random page between [1, total_pages]
-                const random_page = Math.floor(Math.random() * total_pages + 1);
-                console.log("Random page: " + random_page);
-
-                // Send another request to Discover endpoint, this time with page as filter
-                // Grab a random movie from the returned objects
-                const movie_details = axios
-                                        .get(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&page=${random_page}`)
-                                        .then((response) => {
-                                            console.log(response);
-                                        })
-                                        .catch((error) => {
-                                            console.log(error);
-                                        });
             })
             .catch((error) => {
                 console.log(error);
