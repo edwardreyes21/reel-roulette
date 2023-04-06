59a750cb2348321c02f9f4209e92f071e9e711c3
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-04-06 01:04:51 -0700

    Added genre names mapped to genre IDs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,28 @@ function App() {
     const [minYear, setMinYear] = useState(2023);
     const [selectedMovie, setSelectedMovie] = useState(null);
 
+    const genreMap = {
+        "28": "Action",
+        "12": "Adventure",
+        "16": "Animation",
+        "35": "Comedy",
+        "80": "Crime",
+        "99": "Documentary",
+        "18": "Drama",
+        "10751": "Family",
+        "14": "Fantasy",
+        "36": "History",
+        "27": "Horror",
+        "10402": "Music",
+        "9648": "Mystery",
+        "10749": "Romance",
+        "878": "Science Fiction",
+        "10770": "TV Movie",
+        "53": "Thriller",
+        "10752": "War",
+        "37": "Western"
+      };
+
     const handleMinYearChange = (event) => {
         setMinYear(event.target.value);
     }
@@ -111,7 +133,7 @@ function App() {
                 </select>
                 <ul>
                     {genre.map((genre) => (
-                        <li key={genre}> {genre} <button onClick={() => handleRemoveGenre(genre)}>Remove</button></li>
+                        <li key={genre}> {genreMap[genre]} <button onClick={() => handleRemoveGenre(genre)}>Remove</button></li>
                     ))}
                 </ul>
                 <label htmlFor="minYear">Minimum Year:</label>
