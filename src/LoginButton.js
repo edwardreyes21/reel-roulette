472d24d57f159452889fa736b0fb69b2f4da2b97
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-04-21 00:37:54 -0700

    Modified login button to redirect to auth page

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
 
 function LoginButton() {
   return (
-    <BrowserRouter>
-      <div>
-        <Link to='/login'>
-          <button>Login</button>
-        </Link>
-      </div>
-    </BrowserRouter>
+    <div>
+      <a href="/auth/google">Login via Google</a>
+    </div>
   );
 }
 
