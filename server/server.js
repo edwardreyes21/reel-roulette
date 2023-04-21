1a1fceb43f2128052e92ef0ae8a898e8431a4064
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-04-21 00:38:10 -0700

    Added Passport.js and Google OAuth stuff

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const passport = require('passport');
 const path = require('path');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
 
 const app = express();
 
@@ -8,8 +10,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../build')));
 
-console.log("Testing");
-console.log(path.join(__dirname, '../public/index.html'));
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "/auth/google/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  // This function will be called when the user has been authenticated
+  // You can use this callback to save user data to a database, etc.
+  return cb(null, profile);
+}));
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
