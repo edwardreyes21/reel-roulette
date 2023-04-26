0ce663f23241949e13332ee9218e05f6cc074bbb
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-04-25 19:39:31 -0700

    Added mongoose to access user database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
 const passport = require('passport');
 const path = require('path');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.mongodb_connection_string, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+    googleId: { type: String, required: true },
+    email: { type: String, required: true }
+});
+
+const user = mongoose.model('User', userSchema);
+const db = mongoose.connection.useDb('movie_db');
+const users = db.model('User', userSchema);
 
 const app = express();
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../build')));
