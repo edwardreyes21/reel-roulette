aee5382550983b2dfebceccc1e1daf222bb8dea8
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-04-26 19:15:00 -0700

    Changed redirect location if authentication fails

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ function(accessToken, refreshToken, profile, cb) {
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 
 app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
+  passport.authenticate('google', { failureRedirect: '/' }),
   function(req, res) {
     // Successful authentication, redirect home.
     res.redirect('/');
