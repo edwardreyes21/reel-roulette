9907e6d36dbe50369981e917bd0accccc650f3c6
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-05-14 20:58:21 -0700

    Created BrowserRouter for React routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <App />
-  </>
+  <BrowserRouter>
+    <Routes>
+        <Route path="*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
