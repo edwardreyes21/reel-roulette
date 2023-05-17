85f7062aaff6c24aca93a004c6d18a52813db489
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-05-17 16:23:44 -0700

    Added a button to remove movie from watchlist

--- src/Watchlist.js
+++ src/Watchlist.js
@@ -5,11 +5,23 @@ import axios from 'axios';
 function Watchlist() {
     const [watchlist, setWatchlist] = useState(null);
 
+    const handleRemoveMovie = (movieToRemove) => {
+        setWatchlist(watchlist.filter((movie) => movie !== movieToRemove));
+        axios
+            .post('/watchlist/delete-movie', { movieId: movieToRemove._id })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
     useEffect(() => {
         axios
             .get('/api/watchlist')
             .then((response) => {
-                console.log(response);
+                console.log(response.data.watchlist);
                 setWatchlist(response.data.watchlist);
             })
             .catch((error) => {
@@ -25,7 +37,7 @@ function Watchlist() {
                 <div className="watchlist">
                     {
                       watchlist.map((movie) => (
-                        <div key={movie._id}>{movie.title}</div>
+                        <div key={movie._id}>{movie.title} <button onClick={() => handleRemoveMovie(movie)}>Remove Movie</button></div>
                       ))
                     }
                 </div>
