3056777ad62d99fed62e076b09b9ad17e97b4ba6
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-05-17 15:16:13 -0700

    Movies in user's watchlist are now outputted

--- src/Watchlist.js
+++ src/Watchlist.js
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function Watchlist() {
+    const [watchlist, setWatchlist] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get('/api/watchlist')
+            .then((response) => {
+                console.log(response);
+                setWatchlist(response.data.watchlist);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }, []);
+
     return ( 
-        <div>
+        <div className="watchlist-component">
             <h1>Watchlist</h1>
             <Link to="..">Return to Home Page</Link>
+            {watchlist && (
+                <div className="watchlist">
+                    {
+                      watchlist.map((movie) => (
+                        <div key={movie._id}>{movie.title}</div>
+                      ))
+                    }
+                </div>
+            )}
         </div>
     );
 }
