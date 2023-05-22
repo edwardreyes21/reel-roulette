98aa5daaff2284bdb0e7b877643589b63e91d445
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-05-21 19:16:22 -0700

    Added new divs for home page

--- src/App.js
+++ src/App.js
@@ -159,51 +159,59 @@ function App() {
     return (
         <div className="App">
             <LoginButton />
-            <form>
-                <label htmlFor="genre-select">Select genres:</label>
-                <select id="genre-select" multiple={true} size={5} onChange={handleGenreChange}>
-                    <option value="28">Action</option>
-                    <option value="12">Adventure</option>
-                    <option value="16">Animation</option>
-                    <option value="35">Comedy</option>
-                    <option value="80">Crime</option>
-                    <option value="99">Documentary</option>
-                    <option value="18">Drama</option>
-                    <option value="10751">Family</option>
-                    <option value="14">Fantasy</option>
-                    <option value="36">History</option>
-                    <option value="27">Horror</option>
-                    <option value="10402">Music</option>
-                    <option value="9648">Mystery</option>
-                    <option value="10749">Romance</option>
-                    <option value="878">Science Fiction</option>
-                    <option value="10770">TV Movie</option>
-                    <option value="53">Thriller</option>
-                    <option value="10752">War</option>
-                    <option value="37">Western</option>
-                </select>
-                <ul>
-                    {genre.map((genre) => (
-                        <li key={genre}> {genreMap[genre]} <button onClick={() => handleRemoveGenre(genre)}>Remove</button></li>
-                    ))}
-                </ul>
-                <input type="text" id="actorName" placeholder="Enter the name of an actor"/>
-                <ul>
-                    {actors.map((actor) => (
-                        <li key={actor}> {actor.name} <button onClick={() => handleRemoveActor(actor)}>Remove</button></li>
-                    ))}
-                </ul>
-                <button onClick={(event) => handleActorChange(document.getElementById('actorName').value, event)}>Add actor</button>
-                <label htmlFor="minYear">Minimum Year:</label>
-                <input type="text" id="minYear" value={minYear} onChange={handleMinYearChange}/>
-                <label htmlFor="maxYear">Maximum Year:</label>
-                <input type="text" id="maxYear" value={maxYear} onChange={handleMaxYearChange}/>
-            </form>
+            <div className="movie-form">
+                <form>
+                    <div className="genre-select-div">
+                        <label htmlFor="genre-select">Select genres:</label>
+                        <select id="genre-select" multiple={true} size={5} onChange={handleGenreChange}>
+                            <option value="28">Action</option>
+                            <option value="12">Adventure</option>
+                            <option value="16">Animation</option>
+                            <option value="35">Comedy</option>
+                            <option value="80">Crime</option>
+                            <option value="99">Documentary</option>
+                            <option value="18">Drama</option>
+                            <option value="10751">Family</option>
+                            <option value="14">Fantasy</option>
+                            <option value="36">History</option>
+                            <option value="27">Horror</option>
+                            <option value="10402">Music</option>
+                            <option value="9648">Mystery</option>
+                            <option value="10749">Romance</option>
+                            <option value="878">Science Fiction</option>
+                            <option value="10770">TV Movie</option>
+                            <option value="53">Thriller</option>
+                            <option value="10752">War</option>
+                            <option value="37">Western</option>
+                        </select>
+                        <ul>
+                            {genre.map((genre) => (
+                                <li key={genre}> {genreMap[genre]} <button onClick={() => handleRemoveGenre(genre)}>Remove</button></li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="actor-box">
+                        <input type="text" id="actorName" placeholder="Enter the name of an actor"/>
+                        <button onClick={(event) => handleActorChange(document.getElementById('actorName').value, event)}>Add actor</button>
+                        <ul>
+                            {actors.map((actor) => (
+                                <li key={actor}> {actor.name} <button onClick={() => handleRemoveActor(actor)}>Remove</button></li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="year-selection-box">
+                        <label htmlFor="minYear">Minimum Year:</label>
+                        <input type="text" id="minYear" value={minYear} onChange={handleMinYearChange}/>
+                        <label htmlFor="maxYear">Maximum Year:</label>
+                        <input type="text" id="maxYear" value={maxYear} onChange={handleMaxYearChange}/>
+                    </div>
+                </form>
+            </div>
 
-            <button onClick={findMovie}>Find Me a Movie!</button>
+            <button id="find-button" onClick={findMovie}>Find Me a Movie!</button>
 
             {selectedMovie && (
-                <div>
+                <div className="selected-movie">
                     <button onClick={() => handleAddToWatchList()}>Add to Watch List</button>
                     <h1>Title: {selectedMovie.title}</h1>
                     <img id="poster" src={`https://image.tmdb.org/t/p/w500/${selectedMovie.poster_path}`}></img>
